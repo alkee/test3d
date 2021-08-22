3d1100aa4c6762ccb0812464fd3e0bb8f6e80438
alkee <alkee@skia.kr>
2021-08-22 17:11:32 +0900

    initial unity project setup

---
+++ Assets/Scenes/SampleTester.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleTester
+    : MonoBehaviour
+{
+
+
+    #region Unity message handlers
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    #endregion
+}
