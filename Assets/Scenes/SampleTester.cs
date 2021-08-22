1b1bfba075bf1e865273615ba3947b3c577b69f3
alkee <alkee@skia.kr>
2021-08-22 18:14:55 +0900

    sample for .obj loader

--- Assets/Scenes/SampleTester.cs
+++ Assets/Scenes/SampleTester.cs
@@ -1,21 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class SampleTester
     : MonoBehaviour
 {
-
+    public aus.Geometry.WavefrontObjMesh objLoader;
 
     #region Unity message handlers
-    void Start()
+    async void Start()
     {
-        
-    }
-
-    void Update()
-    {
-        
+        var objFilePath = Application.streamingAssetsPath + "/mesh/HeadSample1.obj";
+        await objLoader.LoadAsync(objFilePath);
+        var mesh = objLoader.GetMeshFilter().mesh;
+        Camera.main.transform.position = mesh.bounds.center + Vector3.up * 0.7f;
+        Camera.main.transform.LookAt(mesh.bounds.center);
     }
     #endregion
 }
